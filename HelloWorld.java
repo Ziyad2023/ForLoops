9f870b72ebbe70398e4d8bfdd918d454d13f528f
Ziyad Wakil <ziyad.wakil@mrjaffesclass.com>
2022-10-03 11:49:39 -0700

    Easy. Started not done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,21 @@
-public class HelloWorld 
+public class HelloWorld
 {
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello World!")
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        for (int i = 0; i <= 15; i = i +1) {
+            System.out.println("i is equal to "+i);
+        }
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
     }
 }
